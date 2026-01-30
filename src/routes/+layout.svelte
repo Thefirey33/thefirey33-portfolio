8c96e07663bd7d7893d69e8591ba4a593478aa34
thefirey33 <mmethefirey@gmail.com>
2026-01-31 02:41:13 +0300

    add coding icon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,7 +7,7 @@
 	import homeIcon from '$lib/assets/imgs/icons/homeIcon.webp';
 	import artIcon from '$lib/assets/imgs/icons/artIcon.webp';
 	import scheduleIcon from '$lib/assets/imgs/icons/scheduleIcon.webp';
-
+	import codingIcon from '$lib/assets/imgs/icons/codingIcon.webp';
 
 	import { onNavigate } from '$app/navigation';
 	import IconLinkButton from '$lib/assets/components/IconLinkButton.svelte';
@@ -22,20 +22,20 @@
 	<meta name="keywords" content="portfolio, thefirey33, web developer, software engineer" />
 </svelte:head>
 
-<div class="bg-black w-screen h-screen fixed opacity-80"></div>
+<div class="fixed h-screen w-screen bg-black opacity-80"></div>
 <div class="flex flex-row gap-4 font-[Terminus]">
 	<!--component
 	Very cool navigation bar.
 	-->
 	<nav
-		class="z-50 flex h-screen w-fit flex-col gap-2 border-r border-white bg-black bg-linear-to-bl p-1 select-none"
+		class="z-50 flex h-screen w-fit flex-col gap-2 rounded-r-4xl border-r border-white bg-black bg-linear-to-bl p-5 select-none"
 	>
 		<IconLinkButton link="/" icon={homeIcon} />
 		<IconLinkButton link="/art" icon={artIcon} />
-		<IconLinkButton link="/calendar" icon={scheduleIcon}/>
-		<LinkButton link="/coding">Coding</LinkButton>
+		<IconLinkButton link="/calendar" icon={scheduleIcon} />
+		<IconLinkButton link="/coding" icon={codingIcon} />
 		<footer class="mt-auto flex flex-col gap-4">
-			<button class="btn" onclick={() => window.location.reload()}>Refresh</button>
+			<button class="btn h-10" onclick={() => window.location.reload()}>Refresh</button>
 			<img src={nikoFirey} alt="nikofirey!" draggable="false" class="w-20" />
 		</footer>
 	</nav>
